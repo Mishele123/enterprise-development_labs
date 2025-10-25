977c60427e116c0cee4ea1a1597845baccb263fb
Mishele123 <Misha228665161@gmail.com>
2025-10-26 01:56:38 +0400

    adding reports Interface and dto

---
+++ CarRental.Contracts/Reports/ClientRentalStatsDto.cs
@@ -0,0 +1,17 @@
+﻿namespace CarRental.Application.Contracts.Reports;
+
+/// <summary>
+/// DTO for client rental statistics
+/// </summary>
+/// <param name="ClientId">Client identifier</param>
+/// <param name="FullName">Client full name</param>
+/// <param name="DriverLicenseNumber">Driver license number</param>
+/// <param name="TotalRentalAmount">Total rental amount spent</param>
+/// <param name="RentalCount">Number of rentals</param>
+public record ClientRentalStatsDto(
+    int ClientId,
+    string FullName,
+    string DriverLicenseNumber,
+    decimal TotalRentalAmount,
+    int RentalCount
+);
---
+++ CarRental.Contracts/Reports/CurrentlyRentedCarDto.cs
@@ -0,0 +1,21 @@
+﻿namespace CarRental.Application.Contracts.Reports;
+
+/// <summary>
+/// DTO for currently rented cars
+/// </summary>
+/// <param name="CarId">Car identifier</param>
+/// <param name="LicensePlate">License plate</param>
+/// <param name="ModelName">Car model name</param>
+/// <param name="ClientName">Client who rented the car</param>
+/// <param name="RentalStartTime">Rental start time</param>
+/// <param name="RentalEndTime">Expected rental end time</param>
+/// <param name="RemainingHours">Remaining rental hours</param>
+public record CurrentlyRentedCarDto(
+    int CarId,
+    string LicensePlate,
+    string ModelName,
+    string ClientName,
+    DateTime RentalStartTime,
+    DateTime RentalEndTime,
+    int RemainingHours
+);
